eb63d28d893c914ba4730a7caaf41f8d0b51a9f5
hyun Soo <limitkr277@gmail.com>
2025-08-03 21:39:46 +0900

    first commit

---
+++ 1_basics/9_if_and_switch.js
@@ -0,0 +1,14 @@
+const englishDay = "monday";
+switch (englishDay) {
+  case "monday":
+    koreanDay = "월요일";
+    break;
+  case "Tuesday":
+    koreanDay = "화요일";
+    break;
+  default:
+    koreanDay = "주말";
+    break;
+}
+
+console.log(koreanDay);
